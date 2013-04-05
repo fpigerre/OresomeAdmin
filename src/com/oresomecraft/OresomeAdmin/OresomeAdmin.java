c79fac5203e90b8840e03f9831bf43ba044729d3
Zach De Koning <zach.de.koning@gmail.com>
2013-04-05 21:21:32 +1100

    Grr.. Stupid github...

--- src/com/oresomecraft/OresomeAdmin/OresomeAdmin.java
+++ src/com/oresomecraft/OresomeAdmin/OresomeAdmin.java
@@ -16,10 +16,10 @@ import com.sk89q.bukkit.util.CommandsManagerRegistration;
 import com.sk89q.minecraft.util.commands.*;
 
 public class OresomeAdmin extends JavaPlugin {
-    
+
     private CommandsManager<CommandSender> commands;
     private boolean opPermissions;
-    
+
     public MySQL mysql;
     public String mysql_host;
     public String mysql_port;
@@ -47,9 +47,9 @@ public class OresomeAdmin extends JavaPlugin {
 	getServer().getPluginManager().registerEvents(new GlobalListener(this), this);
 
 	server_name = getConfig().getString("server_name");
-	
+
     }
-    
+
     private void setupDatabase() {
 
 	mysql_host = getConfig().getString("mysql.host");
@@ -70,20 +70,20 @@ public class OresomeAdmin extends JavaPlugin {
 	    if (!mysql.checkTable(table_name)) {
 		getLogger().info("Creating table '"+table_name+"' in database " + getConfig().getString("mysql.database"));
 		mysql.createTable("CREATE TABLE "+table_name+" ( id int NOT NULL AUTO_INCREMENT, server VARCHAR(32) NOT NULL, username VARCHAR(32) NOT NULL, " + 
-		     "reason VARCHAR(32) NOT NULL, ban_date VARCHAR(32) NOT NULL, moderator VARCHAR(32) NOT NULL,"
+			"reason VARCHAR(32) NOT NULL, ban_date VARCHAR(32) NOT NULL, moderator VARCHAR(32) NOT NULL,"
 			+" active VARCHAR(32) NOT NULL, type VARCHAR(32) NOT NULL, PRIMARY KEY (id) ) ENGINE=MyISAM;");
 	    }
-	    
+
 	} else {
 	    getLogger().severe("Error connecting to database, there'll most likely be a lot of console errors!!");
 	}
 	mysql.close();
     }
-    
+
     /**********************************************************************
      * Code to use for sk89q's command framework goes below this comment! *
      **********************************************************************/
-    
+
     private void registerCommands() {
 	final OresomeAdmin plugin = this;
 	// Register the commands that we want to use
@@ -98,33 +98,33 @@ public class OresomeAdmin extends JavaPlugin {
 
 	cmdRegister.register(CommandHandler.class);
     }
-    
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label,
-            String[] args) {
-        try {
-            commands.execute(cmd.getName(), args, sender, sender);
-        } catch (CommandPermissionsException e) {
-            sender.sendMessage(ChatColor.RED + "You don't have permission.");
-        } catch (MissingNestedCommandException e) {
-            sender.sendMessage(ChatColor.RED + e.getUsage());
-        } catch (CommandUsageException e) {
-            sender.sendMessage(ChatColor.RED + e.getMessage());
-            sender.sendMessage(ChatColor.RED + e.getUsage());
-        } catch (WrappedCommandException e) {
-            if (e.getCause() instanceof NumberFormatException) {
-                sender.sendMessage(ChatColor.RED + "Number expected, string received instead.");
-            } else {
-                sender.sendMessage(ChatColor.RED + "An error has occurred. See console.");
-                e.printStackTrace();
-            }
-        } catch (CommandException e) {
-            sender.sendMessage(ChatColor.RED + e.getMessage());
-        }
+	    String[] args) {
+	try {
+	    commands.execute(cmd.getName(), args, sender, sender);
+	} catch (CommandPermissionsException e) {
+	    sender.sendMessage(ChatColor.RED + "You don't have permission.");
+	} catch (MissingNestedCommandException e) {
+	    sender.sendMessage(ChatColor.RED + e.getUsage());
+	} catch (CommandUsageException e) {
+	    sender.sendMessage(ChatColor.RED + e.getMessage());
+	    sender.sendMessage(ChatColor.RED + e.getUsage());
+	} catch (WrappedCommandException e) {
+	    if (e.getCause() instanceof NumberFormatException) {
+		sender.sendMessage(ChatColor.RED + "Number expected, string received instead.");
+	    } else {
+		sender.sendMessage(ChatColor.RED + "An error has occurred. See console.");
+		e.printStackTrace();
+	    }
+	} catch (CommandException e) {
+	    sender.sendMessage(ChatColor.RED + e.getMessage());
+	}
 
-        return true;
+	return true;
     }
-    
+
     public boolean hasPermission(CommandSender sender, String perm) {
 	if (!(sender instanceof Player)) {
 	    if (sender.hasPermission(perm)) {
